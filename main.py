8f6333678d1ee499ea524565525e46974c8e0c54
Jason Surya <57711222+jasonyauw777@users.noreply.github.com>
2023-06-03 22:43:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st 
+from transformers import pipeline
+
+def summarizeText(summarizer, txt):
+    summarized_text = summarizer(txt)[0]["summary_text"]
+    return summarized_text
+
+def main():
+    st.set_page_config(page_title="Automatic Text Summarizer With BART")
+    # BART MODEL DEVELOPED 
+    summarizer = pipeline("summarization", model = "jasonsurya0/BART_TWELVE")
+    # ----HEADER
+    st.subheader("Text Summarizer Built With BART")
+
+    #-----Text Area
+    txt = st.text_area('Input Text to Summarize', '''
+        Amanda: I baked cookies. Do you want some? 
+        Jerry: Sure! 
+        Amanda: I'll bring you tomorrow :-) 
+        ''', height = 180)
+
+    if st.button('Summarize'):
+        #st.write(summarizeText(summarizer, txt))
+        st.text_area('Summarized Text', summarizeText(summarizer, txt), height = 140)
+
+if __name__=="__main__":
+    main()
+
+
