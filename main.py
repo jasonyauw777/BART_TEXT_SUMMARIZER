778be51cdfc4a17b19adaca1fda16ccc8edb6438
Jason Surya <jsurya@assuransiastra>
2023-06-03 23:02:06 +0700

    comment transformers library

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st 
-from transformers import pipeline
+# from transformers import pipeline
 
-def summarizeText(summarizer, txt):
-    summarized_text = summarizer(txt)[0]["summary_text"]
-    return summarized_text
+# def summarizeText(summarizer, txt):
+#     summarized_text = summarizer(txt)[0]["summary_text"]
+#     return summarized_text
 
 def main():
     st.set_page_config(page_title="Automatic Text Summarizer With BART")
     # BART MODEL DEVELOPED 
-    summarizer = pipeline("summarization", model = "jasonsurya0/BART_TWELVE")
+    # summarizer = pipeline("summarization", model = "jasonsurya0/BART_TWELVE")
     # ----HEADER
     st.subheader("Text Summarizer Built With BART")
 
@@ -21,7 +21,7 @@ def main():
 
     if st.button('Summarize'):
         #st.write(summarizeText(summarizer, txt))
-        st.text_area('Summarized Text', summarizeText(summarizer, txt), height = 140)
+        #st.text_area('Summarized Text', summarizeText(summarizer, txt), height = 140)
 
 if __name__=="__main__":
     main()
